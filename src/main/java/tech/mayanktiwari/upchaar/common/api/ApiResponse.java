851d2258b4a30bd806b67efc679b3b831296c63e
Mayank Tiwari <mayanktiwari1212.mt@gmail.com>
2025-11-15 15:43:14 +0530

    feat: added generic api response class

---
+++ src/main/java/tech/mayanktiwari/upchaar/common/api/ApiResponse.java
@@ -0,0 +1,32 @@
+package tech.mayanktiwari.upchaar.common.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private T data;
+    private String message;
+    private String correlationId;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .data(data)
+                .message(message)
+                .build();
+    }
+}
